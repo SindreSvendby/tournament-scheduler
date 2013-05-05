aea6d9f044719c0f76f3ab507882a761098be596
Sindre Svendby <sinsvend@gmail.com>
2013-05-05 12:26:43 +0200

    desgin update

--- app/views/tournaments/signup/_signup_not_registered.php
+++ app/views/tournaments/signup/_signup_not_registered.php
@@ -2,14 +2,14 @@
 
 $baseUrl = get_bloginfo('url');
 
-$html = "<p>";
-$html .= "<form method='post' action='".$baseUrl."/tournaments/add_team/{$tournament->id}'>";
+$html = "<form method='post' action='".$baseUrl."/tournaments/add_team/{$tournament->id}'>";
+$html .= "<p class='signup'>Meld deg på:</p>";
 $html .= MvcFormTagsHelper::hidden_input('tournament_id', array('value' => $tournament->id));
 $html .= MvcFormTagsHelper::hidden_input('player_id1', array('value' => $current_user->ID));
 $html .= "{$current_user->display_name} - ";
 $html .= MvcFormTagsHelper::select_input('player_id2', array('options' => array_merge(array('-1'=>''),$availablePlayers)));
-$html .= '<input type="submit" />';
+$html .= '</br><input type="submit" />';
+$html .= '</p>';
 $html .= '</form>';
-$html .= "</p>";
 echo $html;
 ?>
